5519233c343b20fe8a65a0b03db143b0eb3af303
haaansaaaem <haaansaaaem@gmail.com>
2023-07-17 11:46:37 +0900

    Main 페이지에 상품/북마크리스트 나타나고 북마크 아이콘 토글 가능

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'; // npm install react-router-dom
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { BrowserRouter as Route, Routes } from 'react-router-dom';  // npm install react-router-dom
 import './app.css';
 import Header from './component/Header'
 import Main from './pages/Main'
@@ -9,56 +7,15 @@ import ProductList from './pages/ProductList';
 import Bookmark from './pages/Bookmark';
 
 export default function App() {
-
-  // useEffect(() => {
-  //   fetch('http://cozshopping.codestates-seb.link/api/v1/products ')
-  //     .then(response => {
-  //       if(!response.ok){
-  //         throw new Error(response.statusText);
-  //       }
-  //       return response.json();
-  //     })
-  //     .then(json => console.log(json))
-  //     .catch(err => console.err(err));
-  // }, []);
-
-  const [product, setProduct] = useState([]);
-  const [isBookmark, setIsBookmark] = useState(false);
-
-  useEffect(() => {
-    axios
-      .get("http://cozshopping.codestates-seb.link/api/v1/products", {
-        params: { count: 4 },
-      })
-      .then((response) => {
-        const data = response.data.map((item) => ({
-          ...item,
-          isBookmark: false,
-        }));
-        setProduct(data);
-      })
-      .catch((err) => {
-        console.error("Error", err);
-      });
-  }, []);
-
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route exact path="/" element={
-          <Main product={product}
-                setProduct={setProduct}
-                isBookmark={isBookmark}
-                setIsBookmark={setIsBookmark} />}/>
-        <Route path="/products/list" element={<ProductList />} /> 
-        <Route path="/bookmark" element={<Bookmark />} /> 
+        <Route exact path="/" component={Main} />
+        <Route path="/products/list" component={ProductList} /> 
+        <Route path="/bookmark" component={Bookmark} /> 
       </Routes>
       <Footer />
     </BrowserRouter>
   );
-
-  // function DebugComponent(data){
-  //   return <button onClick={() => console.log(data)}>console.log</button>
-  // }
 }
--- src/util/DebugComponents.js
+++
@@ -1,3 +0,0 @@
-export default function DebugComponent(data){
-  return <button onClick={() => console.log(data)}>console.log</button>
-}
